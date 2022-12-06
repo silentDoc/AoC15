e2ceb8c724af5b3366618cfb83c9e11ef9dc6143
silentDoc <rfelip@gmail.com>
2022-12-06 21:50:03 +0100

    Initial Commit - Day 1 prep

--- AoC15/Program.cs
+++ AoC15/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program _instance = new Program();
+            string input = "";
+            int result = -1;
+
+            int day = 1;
+            int part = 1;
+            bool test = false;
+
+            input = "./Input/day" + day.ToString() + "_1";
+            input += (test) ? "_test.txt" : ".txt";
+
+            Console.WriteLine("AoC 2015 - Day {0} , Part {1} - Test Data {2}", day, part, test);
+            switch (day)
+            {
+                case 1:
+                    result = _instance.Day1(input, part);
+                    Console.WriteLine("Result : {0}", result);
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("Key to exit");
+            Console.ReadLine();
+        }
+
+        int Day1(string input, int part)
+        {
+            var lines = File.ReadLines(input).ToList();
+
+            return 0;
         }
     }
 }
