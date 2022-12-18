97e93acc95452b747cdfe6e098b34a0efc2c508d
silentDoc <ramon78t@yahoo.es>
2022-12-18 01:02:43 +0100

    Day 20 - Part 2 Completed. Fixed a bug in the inner loop to begin with the proper value

--- AoC15/Day20/SantaDeliver.cs
+++ AoC15/Day20/SantaDeliver.cs
@@ -10,36 +10,13 @@ namespace AoC15.Day20
     {
         public int Solve(int minNumPresents, int part = 1)
             => part == 1 ? SolvePart1(minNumPresents) : SolvePart2(minNumPresents);
-
-        int SolvePart1(int minNumPresents)
-            => Deliver(minNumPresents);
-
-        int CalcNumPresents(int houseNumber)
-        { 
-            HashSet<int> divisors = new HashSet<int>();
-            int n = houseNumber;
-            for (int i = 1; i < n / 2; i++)
-            {
-                if (divisors.Contains(i))
-                    continue;
-                if (n % i == 0)
-                { 
-                    divisors.Add(i);
-                    divisors.Add(n / i);
-                }
-            }
-
-            int retVal = 0;
-            foreach (var divisor in divisors)
-                retVal += divisor * 10;
-            return retVal; 
-        }
+               
 
         int Deliver(int minNumPresents)
         {
             var houses = new int[minNumPresents / 10 + 1];
             for (int elf = 1; elf < houses.Length; elf++)
-                for (int house = 0; house < houses.Length; house += elf)
+                for (int house = elf; house < houses.Length; house += elf)
                     houses[house] = houses[house] + 10 * elf;
 
             for (int i = 0; i < houses.Length; i++)
@@ -49,9 +26,31 @@ namespace AoC15.Day20
             return 0;
         }
 
-        int SolvePart2(int minNumPresents)
+        int Deliver2(int minNumPresents)
         {
+            var houses = new int[minNumPresents / 11 + 1];
+            for (int elf = 1; elf < houses.Length; elf++)
+            {
+                int visits = 0;
+                for (int house = elf; (house < houses.Length && visits<=50); house += elf)
+                {
+                    visits++;
+                    houses[house] = houses[house] + 11 * elf;
+                }
+            }
+
+            for (int i = 0; i < houses.Length; i++)
+                if (houses[i] >= minNumPresents)
+                    return i;
+
             return 0;
         }
+
+
+        int SolvePart1(int minNumPresents)
+            => Deliver(minNumPresents);
+
+        int SolvePart2(int minNumPresents)
+            => Deliver2(minNumPresents);
     }
 }
